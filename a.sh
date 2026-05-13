4aa918e79de1f70a8362aee28cb8f52d737ba29a
azeez993300-ux <azeez993300@gmail.com>
2026-05-13 16:57:32 +0000

    update

--- a.sh
+++
@@ -1,9 +0,0 @@
-# Remove the Stripe key from ALL history
-java -jar bfg-1.14.0.jar --replace-text <(echo "sk_") .
-
-# Clean up
-git reflog expire --expire=now --all
-git gc --prune=now --aggressive
-
-# Force push
-git push origin main --force
